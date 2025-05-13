fdf3d11b1c606113c76d125f6d145321705adcf2
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-05-13 16:20:57 -0300

    build: Desenvolvendo o arquivo de contexto do banco de dados.

---
+++ App_Agenda_Fatec/Models/MongoDBContext.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Driver;
+
+namespace App_Agenda_Fatec.Models
+{
+
+    public class MongoDBContext
+    {
+
+        // Atributos de conexão com o banco de dados.
+
+        public static string? Connection_String { get; set; }
+
+        public static string? Database_Name { get; set; }
+
+        public static bool Is_Ssl { get; set; }
+
+        // Atributo de acesso ao banco de dados.
+
+        private IMongoDatabase Database { get; }
+
+        // Método construtor da classe.
+
+        public MongoDBContext()
+        {
+
+            try
+            {
+
+                MongoClientSettings connection_settings = MongoClientSettings.FromUrl(new MongoUrl(Connection_String));
+
+                if (Is_Ssl)
+                {
+
+                    connection_settings.SslSettings = new SslSettings()
+                    {
+
+                        EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12
+
+                    };
+
+                }
+
+                MongoClient connection = new MongoClient(connection_settings);
+
+                this.Database = connection.GetDatabase(Database_Name);
+
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception("Não foi possível se conectar ao MongoDB.", ex);
+
+            }
+
+        }
+
+        // Coleções do banco de dados (MongoDB).
+
+    }
+
+}
