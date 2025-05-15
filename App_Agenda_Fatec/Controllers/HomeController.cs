b205ac54786be45cb96bfeea20fbab70d75c5bae
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-05-14 21:40:38 -0300

    feat: Desenvolvendo as páginas estáticas.

--- App_Agenda_Fatec/Controllers/HomeController.cs
+++ App_Agenda_Fatec/Controllers/HomeController.cs
@@ -6,26 +6,44 @@ namespace App_Agenda_Fatec.Controllers;
 
 public class HomeController : Controller
 {
+
     private readonly ILogger<HomeController> _logger;
+
 
     public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
+
+        this._logger = logger;
+
     }
 
     public IActionResult Index()
     {
+
         return View();
+
     }
 
-    public IActionResult Privacy()
+    public IActionResult Team()
     {
+
         return View();
+
+    }
+
+    public IActionResult Help()
+    {
+
+        return View();
+
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    
     }
+
 }
